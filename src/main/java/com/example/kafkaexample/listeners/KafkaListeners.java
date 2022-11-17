d8b459ebfd723e607da12233c83022181e7c2ff7
Karolina <200626@student.pwr.edu.pl>
2022-11-17 15:56:21 +0100

    Added Listener to receive data from Kafka Cluster

---
+++ src/main/java/com/example/kafkaexample/listeners/KafkaListeners.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexample.listeners;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "new-topic", groupId = "groupId")
+    void listener(String data) {
+        System.out.println("Listener received: " + data );
+    }
+}
